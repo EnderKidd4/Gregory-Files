2c2a89624870ce3aac8e97c289d5e03d5e5eef19
Rayne <rayna7523@gmail.com>
2025-07-24 09:22:17 -0700

    fixed some stuff in the placing functions

--- Project 3/main.py
+++ Project 3/main.py
@@ -97,12 +97,13 @@ def move():
                 time.sleep(1)
 
                 # TODO set coordinates appropriately depending on placement of object
+                #dont touch this code im using it as an example ty
                 AK.setPitchRangeMoving(coordinate['pick'], 90, -90, 90)
                 time.sleep(3)
                 Board.setPWMServoPulse(1, 1000, 500)  # Close paw
                 time.sleep(0.5)
                 pick_up = False  # turn off pick_up flag
-                search_left = True
+                search_left = True #begin searching left
                 init_detect_left()
 
             if search_left:
@@ -114,13 +115,14 @@ def move():
 
                     # TODO Implement placing left mechanism, Hint see if "pick_up"
                     #code based on if"pick_up", changed coordinate to place_left
+                    #changed flags to true or false to begin search to the right
                     AK.setPitchRangeMoving(coordinate['place_left'], 90, -90, 90)
                     time.sleep(3)
                     Board.setPWMServoPulse(1, 1000, 500)  # Close paw
                     time.sleep(0.5)
-                    pick_up = False  # turn off pick_up flag
-                    search_left = True
-                    init_detect_left()
+                    search_left = False  # turn off search_left flag
+                    #search_right = True  #begin searching right?
+                    #init_detect_left()
                     
                 else:
                     print("now going Right\n")
@@ -141,9 +143,9 @@ def move():
                     time.sleep(3)
                     Board.setPWMServoPulse(1, 1000, 500)  # Close paw
                     time.sleep(0.5)
-                    pick_up = False  # turn off pick_up flag
-                    search_right = True
-                    init_detect_right()
+                    search_right = False  # turn off searching flag
+                    #search_right = True
+                    #init_detect_right()
 
                 else:
                     print("now going Left\n")
